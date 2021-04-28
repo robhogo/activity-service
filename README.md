0d7df7e7057533f3a128099a9f74a7b57940de61
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-04-28 14:26:05 +0200

    websocket server added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # activity-service
+
+
+### Used sources
+https://www.red-gate.com/simple-talk/dotnet/net-development/using-apache-kafka-with-net/
+https://socket.io/get-started/chat
--- src/consumer/consume.js
+++ src/consumer/consume.js
@@ -1,7 +1,8 @@
 const Kafka = require("node-rdkafka"); // see: https://github.com/blizzard/node-rdkafka
-const externalConfig = require('./config').config;
+const externalConfig = require('./consumer-config').config;
 
-const CONSUMER_GROUP_ID = "node-consumer-one"
+const CONSUMER_GROUP_ID = "node-consumer-one";
+const server = require("../socket-server/server");
 
 const kafkaConf = {
     "group.id": CONSUMER_GROUP_ID,
@@ -28,6 +29,9 @@ stream.on('data', function (message) {
     //     key: 'someKey', // key of the message if present
     //     timestamp: 1510325354780 // timestamp of message creation
     //   }
+    server.io.on('connection', (socket) => {
+        socket.emit('chat message', input.value);
+    });
 });
 
 console.log(`Stream consumer created to consume from topic ${topics}`);
