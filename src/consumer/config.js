5cf24380ea0a08ad03f6b795e6881fa7c5e60dc7
JaapvanderMeer <43168682+JeapfromtheLake@users.noreply.github.com>
2021-04-28 13:16:54 +0200

    gitignore added, consumer project started

---
+++ src/consumer/config.js
@@ -0,0 +1,6 @@
+//Configuration Details for the Kafka Brokers and Topic
+const config = {
+    KAFKA_BROKERS: "localhost:9092",
+    KAFKA_TOPIC: "simpletalk_topic:1:1"
+};
+module.exports = { config };
