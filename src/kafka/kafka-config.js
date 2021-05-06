e75ee23c176505a294b7fe51422e2ec9d1c0fbc6
Aron Heesakkers <aron.heesakkers@via.nl>
2021-05-06 12:59:19 +0200

    Fixed

--- src/kafka/kafka-config.js
+++ src/kafka/kafka-config.js
@@ -1,6 +1,6 @@
 //Configuration Details for the Kafka Brokers and Topic
 const config = {
-    KAFKA_BROKERS: "localhost:9092",
+    KAFKA_BROKERS: "0.0.0.0:9092",
     KAFKA_TOPIC: "simpletalk_topic"
 };
 module.exports = { config };
